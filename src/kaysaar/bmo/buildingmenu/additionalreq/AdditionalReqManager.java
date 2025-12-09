88e4f7f439872dcbb6978676c975cbd713bc539c
Wiktor Heblewski <240681@edu.p.lodz.pl>
2025-12-09 04:25:20 +0100

    bmo additional req rework

--- src/kaysaar/bmo/buildingmenu/additionalreq/AdditionalReqManager.java
+++ src/kaysaar/bmo/buildingmenu/additionalreq/AdditionalReqManager.java
@@ -4,11 +4,12 @@ import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.campaign.econ.MarketAPI;
 
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 
 public class AdditionalReqManager {
     //Note : this is used by aotd this is why there is only one instance of baseAdditionalReq use it ONLY for vanilla industries
     public static AdditionalReqManager instance;
-    public LinkedHashMap<String,BaseAdditionalReq>reqLinkedHashMap = new LinkedHashMap<>();
+    public LinkedHashSet<BaseAdditionalReq>reqLinkedHashMap = new LinkedHashSet<>();
     public static AdditionalReqManager getInstance() {
         if (instance == null) {
             instance = new AdditionalReqManager();
@@ -16,21 +17,34 @@ public class AdditionalReqManager {
         }
         return instance;
     }
-    public void  addReq(String id,BaseAdditionalReq req){
-        reqLinkedHashMap.put(id,req);
+    public void  addReq(BaseAdditionalReq req){
+        reqLinkedHashMap.add(req);
     }
 
-    public LinkedHashMap<String, BaseAdditionalReq> getReqLinkedHashMap() {
+    public LinkedHashSet<BaseAdditionalReq> getReqLinkedHashMap() {
         return reqLinkedHashMap;
     }
-    public BaseAdditionalReq getReq(String id) {
-        return reqLinkedHashMap.get(id);
-    }
-    public boolean doesMetReq(String id, MarketAPI market) {
-        if(!reqLinkedHashMap.containsKey(id)){
-            return true;
+    public boolean doesMetReq(String industryId, MarketAPI market) {
+        for (BaseAdditionalReq value : reqLinkedHashMap) {
+            if(value.getIndustriesAffected().contains(industryId)){
+                if(!value.metCriteria(market,industryId))return false;
+            }
         }
-        BaseAdditionalReq req = reqLinkedHashMap.get(id);
-        return req.metCriteria(market,id);
+        return true;
+    }
+    public String getAdditionalReasons(String industryId, MarketAPI market) {
+        StringBuilder builder = new StringBuilder();
+        boolean appended = false;
+        for (BaseAdditionalReq value : reqLinkedHashMap) {
+            if(value.getIndustriesAffected().contains(industryId)){
+                if(!value.metCriteria(market,industryId))continue;
+                if(appended){
+                    builder.append("\n");
+                }
+                builder.append(value.getReason(market,industryId));
+                appended = true;
+            }
+        }
+        return builder.toString();
     }
 }
--- src/kaysaar/bmo/buildingmenu/additionalreq/BaseAdditionalReq.java
+++ src/kaysaar/bmo/buildingmenu/additionalreq/BaseAdditionalReq.java
@@ -3,6 +3,8 @@ package kaysaar.bmo.buildingmenu.additionalreq;
 import com.fs.starfarer.api.campaign.econ.Industry;
 import com.fs.starfarer.api.campaign.econ.MarketAPI;
 
+import java.util.ArrayList;
+
 public class BaseAdditionalReq {
     public boolean metCriteria(MarketAPI market, String indId){
         return true;
@@ -10,4 +12,8 @@ public class BaseAdditionalReq {
     public String getReason(MarketAPI market, String indId){
         return "";
     }
+    public ArrayList<String> getIndustriesAffected(){
+        return new ArrayList<>();
+    }
+
 }
